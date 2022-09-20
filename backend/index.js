7c3f1acf422e381e070c497f6fa859edc56730c1
andreneox <103339143+andreneox@users.noreply.github.com>
2022-09-20 10:41:45 -0400

    Inicio, configuracao e teste para rota SCORES

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,38 @@ app.get("/", function (req, res) {
   res.send("Hello, World!");
 });
 
-//criando endpoints para backend    
+//criando endpoints para backend (para teste)   
 app.get("/oi", function (req, res) {
   res.send("Olá, mundo!");
 });
 
-app.listen(3000);
+
+// Nosso backend vai armazenar as pontuações das jogadas
+// Criando  uma lista  para teste com as pontuações
+const lista = [
+    {
+      id: 1,
+      nome: "And",
+      pontos: 23,
+    },
+    {
+      id: 2,
+      nome: "John",
+      pontos: 63,
+    },
+    {
+      id: 3,
+      nome: "Maria",
+      pontos: 96,
+    },
+  ];
+  
+  // Endpoint SCORES - READ ALL - [GET] /scores
+  app.get("/scores", function (req, res) {
+    res.send("Ler todas as pontuações-scores");
+  });
+  
+  
+  
+  app.listen(3000);
+
