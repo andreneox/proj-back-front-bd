04cc8c507dcb825cac80ab5536f5d7bdafb0e73b
andreneox <103339143+andreneox@users.noreply.github.com>
2022-09-23 13:43:18 -0400

    Estilizando a nuvem (clouds)

--- frontend/src/components/game/Game.js
+++ frontend/src/components/game/Game.js
@@ -2,11 +2,11 @@ import "./Jogo.css";
 import clouds from "../../assets/clouds.png";
 
 function Jogo() {
-  return (
-    <div className="jogo">
-      <img src={clouds} />
-    </div>
-  );
-}
+    return (
+      <div className="jogo">
+        <img className="nuvens" src={clouds} alt="Nuvens" />
+      </div>
+    );
+  }
 
 export default Jogo;
