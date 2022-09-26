2e4bd5ce506d84b38c6eae048324d248be50c3a3
andreneox <103339143+andreneox@users.noreply.github.com>
2022-09-26 10:01:49 -0400

    Configurando o estado (useState) estaPulando, com confirmação do botao clicado  no onkeydown mostrando no console.log

--- frontend/src/components/game/Game.js
+++ frontend/src/components/game/Game.js
@@ -2,8 +2,23 @@ import "./Jogo.css";
 import nuvens from "../../assets/clouds.png";
 import cano from "../../assets/pipe.png";
 import mario from "../../assets/mario.gif";
+import {useState} from "react"; 
+
 
 function Jogo() {
+
+ /*configurando o estado (useState) estaPulando, com confirmação do botao clicado 
+ no onkeydown mostrando no console.log*/ 
+const [estaPulando, setEstaPulando] = useState (false);
+
+document.onkeydown = function(){
+  console.log ("On Key Down");
+
+  estaPulando = true;
+};
+
+console.log(15, {useState} );
+
   return (
     <div className="jogo">
       <img className="nuvens" src={nuvens} alt="Nuvens" />
